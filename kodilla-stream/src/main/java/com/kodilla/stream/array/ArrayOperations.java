b1bbb5e3cb26c649eed17c7a5559ca7e88446616
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2020-10-17 12:18:56 +0100

    7.5 and 7.6 - maintenance

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,6 +4,9 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .map(i -> numbers [i])
+                .forEach(System.out::println);
         return IntStream.range(0, numbers.length)
                 .map(s -> numbers[s])
                 .average()
